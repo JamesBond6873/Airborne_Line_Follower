e4eff32e4f09a020430feb5fc9d67b6e2c4e39b8
James Bond 73 <110297851+JamesBond6873@users.noreply.github.com>
2024-04-21 22:01:12 +0100

    Francisco: Added teste_motores.ino to software_test. Spins the motor forward 1sec and backward 1sec

---
+++ Software_Test/teste_motores/teste_motores.ino
@@ -0,0 +1,38 @@
+#define RF 18 // Right Motor Forward Pin
+#define RB 19 // Right Motor Backward Pin
+#define LF 20 // Left Motor Forward Pin
+#define LB 21 // Left Motor Backward Pin
+
+#define Led 25
+
+void setup() {
+  pinMode(RF, OUTPUT);
+  pinMode(RB, OUTPUT);
+  pinMode(LF, OUTPUT);
+  pinMode(LB, OUTPUT);
+
+  pinMode(Led, OUTPUT);
+
+  // Set Stop motions
+  digitalWrite(RF, LOW);
+  digitalWrite(RB, LOW);
+  digitalWrite(LF, LOW);
+  digitalWrite(LB, LOW);
+}
+
+void loop() {
+  digitalWrite(Led, HIGH);
+  digitalWrite(LB, LOW);
+  digitalWrite(RB, LOW);
+  digitalWrite(LB, LOW);
+  digitalWrite(RF, HIGH);
+  digitalWrite(LF, HIGH);
+  delay(1000);
+
+  digitalWrite(Led, LOW);
+  digitalWrite(RF, LOW);
+  digitalWrite(LF, LOW);
+  digitalWrite(RB, HIGH);
+  digitalWrite(LB, HIGH);
+  delay(1000);
+}
